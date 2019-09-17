2e8846b34ae36ab211536e3508ecccfa94dbe322
Eric Loudon <loudon.eric@gmail.com>
2019-09-17 12:33:15 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,1 +1,8 @@
-INSERT INTO bears (name, age, color, temperament, alive) VALUES
+INSERT INTO bears (name, age, color, temperament, alive) VALUES ("Mr. Chocolate", 2, "Brown", "Happy", "Yes")
+INSERT INTO bears (name, age, color, temperament, alive) VALUES ("Rowdy", 4, "Black", "Sad", "Yes")
+INSERT INTO bears (name, age, color, temperament, alive) VALUES ("Tabitha", 6, "White", "Bossy", "Yes")
+INSERT INTO bears (name, age, color, temperament, alive) VALUES ("Sergeant Brown", 8, "Blue", "Noisy", "Yes")
+INSERT INTO bears (name, age, color, temperament, alive) VALUES ("Melissa", 10, "Red", "Sleepy", "Yes")
+INSERT INTO bears (name, age, color, temperament, alive) VALUES ("Grinch", 12, "Green", "Stubborn", "Yes")
+INSERT INTO bears (name, age, color, temperament, alive) VALUES ("Wendy", 14, "Yellow", "Angry", "Yes")
+INSERT INTO bears (name, age, color, temperament, alive) VALUES (NULL, NULL, "Purple", "Hungry", "Yes")
